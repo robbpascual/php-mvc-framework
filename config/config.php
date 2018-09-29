31168ec6051d97fab88ceadc788b85e5a3f17bad
Robb Pascual <43700738+robbpascual@users.noreply.github.com>
2018-09-29 23:20:07 +0800

    add configuration

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-return array(
-	'host'     => 'localhost',
-	'username' => 'root',
-	'password' =>  '',
-	'dbname'   => 'test'
-	); 
-?>
--- config/dir.php
+++ config/dir.php
@@ -1,9 +1,5 @@
 <?php
 
-define('SRCROOT', dirname(__FILE__, 1)); // Source Root
-define('URLROOT',  'http://localhost/php-mvc'); // URL Root
-// define('URLROOT', dirname($_SERVER['PHP_SELF']); same as above
-define('SITENAME', 'Site'); // Site Name
-define('APPVERSION', '1.0.0'); // App Version
+
 
 ?>
